bbec9f7a1212e2b20a468e37455cba178735e53b
Cesar Ibarra <ibarratcesar@icloud.com>
2025-03-16 14:08:29 -0700

    Initial Commit

---
+++ PayCheckCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PayCheckCalculator
+//
+//  Created by Cesar Ibarra on 3/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PayCheckCalculator/PayCheckCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayCheckCalculatorApp.swift
+//  PayCheckCalculator
+//
+//  Created by Cesar Ibarra on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct PayCheckCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
